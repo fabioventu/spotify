8a2fa7112d74281c95195b9e5724f3a29391be0a
fabioventu <venturini.fabio@iisgalvanimi.edu.it>
2020-12-09 10:49:44 +0000

    01.0

---
+++ first-routed-app/src/app/spotify.service.ts
@@ -0,0 +1,25 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+//Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+   //url per oauth: https://developer.spotify.com/console/get-search-item/
+  //Ottengo il modulo HttpClient
+  constructor(private http: HttpClient) { }
+
+  searchTrack(query: string) {
+    const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
+    const headers = new HttpHeaders({
+      Authorization:
+        'Bearer BQDr0HbrboLzC0k7eH3nUkvTvoNIslmcsx0Fam9qnNeTn2E-VlxIwgydZtK6HWyeptgDI5Vjb4hssDo6ZbwvmpplWL3SazcwDjF53E5VlFYpDmdFwM4r9KIckgyBo4wurgCmNfmN2K8Tya3HhnPS'
+    });
+
+    let obsTracks = this.http.get(url, { headers });
+    return obsTracks;
+ //Ritorno un observable ai componenti che richiedono il servizio
+  }
+}
